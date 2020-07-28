0f1ed25db535763659f1fb4ce840090a816b6652
Roman Garanin <git@romangaranin.net>
2020-07-28 22:29:16 +0300

    feat: excess imports removed, F added

--- django/experiments/polls/views.py
+++ django/experiments/polls/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.db.models import F
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
